2f4da233c348a5502dac63bd943facccfef9c6ab
Maxime Elomari <764791+noglitchyo@users.noreply.github.com>
2019-07-19 17:22:35 +0200

    Added copyrights to every files, fixed tests

--- src/DependencyInjection/NoGlitchYoMiddlewareExtension.php
+++ src/DependencyInjection/NoGlitchYoMiddlewareExtension.php
@@ -1,4 +1,27 @@
 <?php declare(strict_types=1);
+/**
+ * MIT License
+ *
+ * Copyright (c) 2019 Maxime Elomari
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace NoGlitchYo\MiddlewareBundle\DependencyInjection;
 
@@ -14,6 +37,10 @@ class NoGlitchYoMiddlewareExtension extends Extension
     {
         $mainConfig = $this->getConfiguration($configs, $container);
 
+        if ($mainConfig === null) {
+            return;
+        }
+
         $configuration = $this->processConfiguration($mainConfig, $configs);
 
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
@@ -26,7 +53,10 @@ class NoGlitchYoMiddlewareExtension extends Extension
                         if (!isset($handlerConfiguration['collection'])) {
                             throw new InvalidArgumentException('Please provide a collection for handler');
                         }
-                        $container->setParameter(sprintf('middlewares.handlers.%s', $handlerName), $handlerConfiguration);
+                        $container->setParameter(
+                            sprintf('middlewares.handlers.%s', $handlerName),
+                            $handlerConfiguration
+                        );
                     }
 
                     $container->setParameter('middlewares.handlers', $config);
--- src/HttpKernel/MiddlewareStackKernel.php
+++ src/HttpKernel/MiddlewareStackKernel.php
@@ -1,4 +1,27 @@
 <?php declare(strict_types=1);
+/**
+ * MIT License
+ *
+ * Copyright (c) 2019 Maxime Elomari
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace NoGlitchYo\MiddlewareBundle\HttpKernel;
 
@@ -7,8 +30,6 @@ use NoGlitchYo\MiddlewareBundle\Middleware\Stack\MiddlewareStackInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
-use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
 use Symfony\Bridge\PsrHttpMessage\HttpFoundationFactoryInterface;
 use Symfony\Bridge\PsrHttpMessage\HttpMessageFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -68,18 +89,19 @@ class MiddlewareStackKernel implements HttpKernelInterface
 
     private function getHttpKernelAsRequestHandler()
     {
-        return new class($this->httpKernel, $this->httpFoundationFactory, $this->psrHttpFactory) implements RequestHandlerInterface
+        return new class($this->httpKernel, $this->httpFoundationFactory, $this->psrHttpFactory)
+            implements RequestHandlerInterface
         {
             /**
              * @var HttpKernelInterface
              */
             private $httpKernel;
             /**
-             * @var HttpFoundationFactory
+             * @var HttpFoundationFactoryInterface
              */
             private $foundationFactory;
             /**
-             * @var PsrHttpFactory
+             * @var HttpMessageFactoryInterface
              */
             private $psrHttpFactory;
 
